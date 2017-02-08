df9fcb916b71d8fd6586105155d723c6b7a1bb6e
Andrew Hite <andrew@andrew-hite.com>
2017-02-08 13:52:33 -0600

    Avoid multiple copies of react being loaded

--- packages/react-scripts/config/webpack.config.prod.js
+++ packages/react-scripts/config/webpack.config.prod.js
@@ -96,6 +96,9 @@ module.exports = {
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
       'react-native': 'react-native-web'
+      // Avoid multiple copies of react being loaded
+      'react': path.join(paths.appNodeModules, 'react'),
+      'react-dom': path.join(paths.appNodeModules, 'react-dom')
     }
   },
   // @remove-on-eject-begin
